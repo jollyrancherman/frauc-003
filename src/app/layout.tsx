e32df65e8a2c4c9e54314c234dfacc692c69aacd
jollyrancherman <anthony.rsc@gmail.com>
2024-09-23 11:36:57 -0700

    Test footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,17 +7,6 @@ import SignInModal from '@/features/auth/components/SignInModal';
 import { ToastContainer } from 'react-toastify';
 import { Providers } from '@/providers/Provider';
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
-
 export const metadata: Metadata = {
   title: 'Frauc',
   description: 'Free local auction',
@@ -30,16 +19,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         {' '}
         <Providers>
           <div className='flex flex-col  h-screen'>
             <div className='flex-grow bg-neutral-50 overflow-auto'>
               {children}
             </div>
-            <div className='absolute bottom-0 w-full'>
+            <div className='absolute bottom-0 left-0 w-full'>
               <FooterNav />
               <SignInModal />
               <ToastContainer />
